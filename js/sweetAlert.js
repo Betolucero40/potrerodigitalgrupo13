e10ab63a085c2adf23947209b60f4c477bb0ec3b
lucero alberto <negrocrack30@gmail.com>
2021-02-18 03:41:56 -0300

    final

---
+++ js/sweetAlert.js
@@ -0,0 +1,19 @@
+Swal.fire({
+  title: "SOS MAYOR DE 18 AÑOS?",
+  showConfirmButton: true,
+  confirmButtonText: 'SI',
+	confirmButtonAriaLabel: 'confirmar',
+  showCancelButton: true,
+	cancelButtonText: 'NO',
+	cancelButtonAriaLabel: 'cancelar',
+  background: '#000',
+  grow: 'row',
+  allowOutsideClick: false,
+  allowEscapeKey: false,
+  allowEnterKey: false,
+  stopKeydownPropagation: false,
+  imageUrl:'img/logo1.jpg',
+	imageWidth: '250px',
+	imageAlt: 'chulengo logo',
+
+});
